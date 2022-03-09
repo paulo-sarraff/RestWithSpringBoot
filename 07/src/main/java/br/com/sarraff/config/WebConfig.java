f2bd7cb0fa3c67fdbaa90058fa2668b4a4354996
Paulo Sarraff Pereira Junior <paulo.junior@deal.com.br>
2022-03-09 16:50:51 -0300

    Atualização do content negotiation (extensão, url e header)

--- 07/src/main/java/br/com/sarraff/config/WebConfig.java
+++ 07/src/main/java/br/com/sarraff/config/WebConfig.java
@@ -13,12 +13,29 @@ public class WebConfig implements WebMvcConfigurer{
 	
 	@Override
 	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
-		configurer.favorParameter(false)
-			.ignoreAcceptHeader(false)
-			.defaultContentType(MediaType.APPLICATION_JSON)
-			.mediaType("json", MediaType.APPLICATION_JSON)
-			.mediaType("xml", MediaType.APPLICATION_XML);
-
+		/* content negotiation via extensão: localhost:8080/api/person/v1.json
+		 * configurer.favorParameter(false) .ignoreAcceptHeader(false)
+		 * .defaultContentType(MediaType.APPLICATION_JSON) .mediaType("json",
+		 * MediaType.APPLICATION_JSON) .mediaType("xml", MediaType.APPLICATION_XML);
+		 */
+		
+		
+		//content negotiation via url: localhost:8080/api/person/v1?mediaType=json
+		/*configurer.favorPathExtension(false)
+		.favorParameter(true)
+		.parameterName("mediaType")
+		.ignoreAcceptHeader(true)
+		.useRegisteredExtensionsOnly(false)
+		.defaultContentType(MediaType.APPLICATION_JSON) .mediaType("json", MediaType.APPLICATION_JSON) 
+		.mediaType("xml", MediaType.APPLICATION_XML);*/
+		
+		//content negotiation via header: colocar parâmetros no header (Accept = application/json)
+		configurer.favorPathExtension(false)
+		.favorParameter(false)
+		.ignoreAcceptHeader(false)
+		.useRegisteredExtensionsOnly(false)
+		.defaultContentType(MediaType.APPLICATION_JSON) .mediaType("json", MediaType.APPLICATION_JSON) 
+		.mediaType("xml", MediaType.APPLICATION_XML);
 	}
 
 }
