1efa27c941dfcf814058747a935966081c164aa1
Paulo Sarraff Pereira Junior <paulo.junior@deal.com.br>
2022-03-11 10:40:36 -0300

    Incluindo hateoas na api

---
+++ 09/src/main/java/br/com/sarraff/Startup.java
@@ -0,0 +1,18 @@
+package br.com.sarraff;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan
+public class Startup {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Startup.class, args);
+	}
+
+}
